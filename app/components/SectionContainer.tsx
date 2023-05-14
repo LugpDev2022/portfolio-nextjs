8b9c857123be47538015ec1b6afb2b25e7cbbedd
LugpDev2022 <lugpdev2022@gmail.com>
2023-05-13 20:16:20 -0600

    Resize the mobile section paddings

--- app/components/SectionContainer.tsx
+++ app/components/SectionContainer.tsx
@@ -4,7 +4,9 @@ interface Props {
 }
 
 const SectionContainer: React.FC<Props> = ({ children, className = '' }) => (
-  <section className={`sm:py-15 px-10 py-10 ${className}`}>{children}</section>
+  <section className={`sm:py-15 px-7 py-10 lg:px-10 ${className}`}>
+    {children}
+  </section>
 );
 
 export default SectionContainer;
